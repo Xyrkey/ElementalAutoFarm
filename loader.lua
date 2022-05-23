7b7329a4cd9f311f657907ee4eae729a89c284d5
Xyrkey <92700301+Xyrkey@users.noreply.github.com>
2022-05-23 10:47:40 +0800

    Create loader.lua

---
+++ loader.lua
@@ -0,0 +1,8 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Xyrkey/ElementalAwaken/main/InfRangeAndInstaSkill/loader.lua"))()
+    wait()
+spawn(function()
+    wait()
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(1176.85193, -180.317245, 1743.93469, -0.999869525, -4.14688657e-08, -0.0161521453, -4.26046363e-08, 1, 6.9972856e-08, 0.0161521453, 7.06518861e-08, -0.999869525)
+    wait()
+end)
+end
