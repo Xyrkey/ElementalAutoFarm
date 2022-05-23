5821ea1d46a841922ebe1b82d817c2f70ba95d61
Xyrkey <92700301+Xyrkey@users.noreply.github.com>
2022-05-23 10:51:49 +0800

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -6,4 +6,3 @@ spawn(function()
 game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(1176.85193, -180.317245, 1743.93469, -0.999869525, -4.14688657e-08, -0.0161521453, -4.26046363e-08, 1, 6.9972856e-08, 0.0161521453, 7.06518861e-08, -0.999869525)
     wait()
 end)
-end
