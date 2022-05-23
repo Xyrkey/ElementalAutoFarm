cead4504d74d8239faabb2c592f4ae16f90d4e37
Xyrkey <92700301+Xyrkey@users.noreply.github.com>
2022-05-23 10:52:48 +0800

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,8 +1,10 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Xyrkey/ElementalAwaken/main/InfRangeAndInstaSkill/loader.lua"))()
 loadstring(game:HttpGet(('https://raw.githubusercontent.com/Aidez/elemental_awakening/main/level_farm'),true))()
+while true do
     wait()
 spawn(function()
     wait()
 game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(1176.85193, -180.317245, 1743.93469, -0.999869525, -4.14688657e-08, -0.0161521453, -4.26046363e-08, 1, 6.9972856e-08, 0.0161521453, 7.06518861e-08, -0.999869525)
     wait()
 end)
+end
